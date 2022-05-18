48ca40463e873d355a1017d5283193fe496c9593
TMysiv <tarasmysiv@gmail.com>
2022-05-18 21:12:38 +0300

    Add HomeWork_3

---
+++ src/app/app-routing.modules.ts
@@ -0,0 +1,24 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+import {HomeComponent} from "./components/home/home.component";
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule)},
+  {path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)},
+  {path: 'comments', loadChildren: () => import('./comments/comments.module').then(m => m.CommentsModule)}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+
+export class AppRoutingModules {
+}
